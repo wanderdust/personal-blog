61ca5c7e718b0fc42db49677221f64aed293c78b
Pablo Lopez <pablo.lopez.santori@gmail.com>
2023-01-14 22:37:16 +0000

    Make message box more structured

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-
 export default function ChatWindow() {
     const [chatMessages, addMessageToChat] = useState([]);
     const [message, setMessage] = useState("");
@@ -10,6 +9,8 @@ export default function ChatWindow() {
 
     const sendMessage = () => {
         const api = "https://2ptgmvb0xk.execute-api.eu-west-1.amazonaws.com/prod/chat/"
+        addMessageToChat(chatMessages => [...chatMessages, messageBubble(message, "right")])
+        setMessage("")
         setIsLoading(true)
 
         axios.get(api, {
@@ -17,16 +18,24 @@ export default function ChatWindow() {
                 message: message
             },
         })
-            .then((response) => addMessageToChat(chatMessages.concat(response.data)))
-            .then(() => setMessage(""))
+            .then((response) => addMessageToChat(chatMessages => [...chatMessages, messageBubble(response.data, "left")]))
             .then(() => setIsLoading(false))
             .catch((error) => console.log(error))
     }
 
+    const messageBubble = (message, position = "right") => {
+        return (
+            <div className={`mb-4 mx-2 text-${position}`}>
+                <span class="text-gray-700">{message}</span>
+            </div>
+        )
+
+    }
+
     return (
         <div className="h-96 w-64 border">
-            <div className="h-[80%] border">
-                {chatMessages.map((message) => <p>{message}</p>)}
+            <div className="h-[80%] border flex flex-col overflow-scroll">
+                {chatMessages.map((message) => <span>{message}</span>)}
             </div>
             <div className="h-[20%] flex flex-row">
                 <input type="text" className="w-[80%]" onChange={(e) => setMessage(e.target.value)} value={message} placeholder="Message here ..." />
