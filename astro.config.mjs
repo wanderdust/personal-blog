6318cedb1ad7db05a1500fbe0063bdcf020b9aa4
Pablo Lopez <pablo.lopez.santori@gmail.com>
2023-01-15 17:52:03 +0000

    Remove comment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
 
 // https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
 });
