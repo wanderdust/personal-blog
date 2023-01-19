adc86f4e2ba369916d1e5748c98f1a9bbee12701
Pablo Lopez <pablo.lopez.santori@gmail.com>
2023-01-19 20:39:13 +0000

    Add mdx config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
 
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), mdx()],
+  markdown: {
+    syntaxHighlight: 'prism',
+    gfm: true,
+  },
 });
